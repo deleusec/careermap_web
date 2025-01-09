a883bc78e09e0f40a3a97269a69eecc160d261df
Corentin Deleuse <dlse.corentin@gmail.com>
2025-01-09 13:41:11 +0100

    feat: implement shadcn config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,12 +10,13 @@ import AppGradientText from '@/components/ui/AppGradientText.vue';
 
       <h1 class="text-4xl text-center font-medium"
       >Cherche ta <AppGradientText>CareerMap</AppGradientText> et trouve ta voie</h1>
-      <p class="text-xl">De la formation au métier de tes rêves, découvre chaque étape de ton futur !</p>
+      <p class="text-base mt-4">De la formation au métier de tes rêves, découvre chaque étape de ton futur !</p>
       <RouterLink to="/roadmap/1" class="mt-4 text-blue-500 underline">Go to Roadmap</RouterLink>
     </div>
 
     <div class="flex flex-col items-center justify-center min-h-[calc(100vh-60px)]">
       <LogoNeonIcon class="w-52 h-52" />
+
     </div>
   </AppLayout>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate"
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         'sans': ['Barlow', 'sans-serif'],
@@ -41,7 +50,21 @@ export default {
       borderRadius: {
         'DEFAULT': 'var(--radius)',
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [animate],
 }
