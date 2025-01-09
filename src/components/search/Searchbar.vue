09a2928730c7fe44a2747617d82ff84cb0dc0184
MC <marie-claire.lambo@edu.devinci.fr>
2025-01-09 12:17:22 +0100

    ✨ searchbar

---
+++ src/components/search/Searchbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative w-auto mt-20">
+    <div class="relative">
+      <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+        <svg class="h-5 w-5 text-foreground" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+        </svg>
+      </div>
+      
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Recherche un métier, une formation ou une catégorie..."
+        class="w-[600px] pr-10 pl-3 py-2 border border-foreground rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+        @input="handleSearch"
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+const emit = defineEmits(['search'])
+
+const handleSearch = () => {
+  emit('search', searchQuery.value)
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,21 @@
-<script setup lang="ts">
-import AppLayout from '@/components/layouts/AppLayout.vue';
-</script>
-
 <template>
   <AppLayout>
-    <div class="flex flex-col items-center justify-center min-h-[calc(100vh-60px)]">
+    <div class="flex flex-col items-center justify-center content-center min-h-[calc(100vh-60px)]">
 
       <h1 class="text-4xl text-center font-medium"
       >Cherche ta <span>CareerMap</span> et trouve ta voie</h1>
       <p class="text-xl">De la formation au métier de tes rêves, découvre chaque étape de ton futur !</p>
+      <Searchbar @search="onSearch"/>
       <RouterLink to="/roadmap/1" class="mt-4 text-blue-500 underline">Go to Roadmap</RouterLink>
     </div>
   </AppLayout>
 </template>
 
+<script setup lang="ts">
+import AppLayout from '@/components/layouts/AppLayout.vue';
+import Searchbar from '@/components/search/Searchbar.vue';
+
+const onSearch = (query: string) => {
+  console.log('Recherche :', query)
+}
+</script>
